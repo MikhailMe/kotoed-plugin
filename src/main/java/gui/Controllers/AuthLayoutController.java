0dbecd3f8c0d216587ac389a4a8797292a803fb8
Boris Legkun <dizamino@gmail.com>
2018-10-24 13:31:52 +0300

    for backup - need controller fix

---
+++ src/main/java/gui/Controllers/AuthLayoutController.java
@@ -0,0 +1,29 @@
+package gui.Controllers;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class AuthLayoutController {
+    /*@FXML
+    private Button pressedButton;
+    @FXML
+    private Label label;*/
+    public AuthLayoutController() {
+    }
+    /*
+    @FXML
+    private void LogInButtonHandler(ActionEvent event)
+    {
+        label.setText("Button is pressed!");
+    }
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+
+    }*/
+}
---
+++ src/main/java/gui/MainWindowFactory.java
@@ -0,0 +1,43 @@
+package gui;
+
+import com.intellij.openapi.project.Project;
+import com.intellij.openapi.wm.ToolWindow;
+import com.intellij.openapi.wm.ToolWindowFactory;
+
+import com.intellij.ui.content.Content;
+import com.intellij.ui.content.ContentFactory;
+import com.sun.javafx.application.PlatformImpl;
+import javafx.application.Platform;
+import javafx.embed.swing.JFXPanel;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import org.jetbrains.annotations.NotNull;
+
+import java.io.IOException;
+
+public class MainWindowFactory implements ToolWindowFactory {
+    private JFXPanel jFXPanel;
+    @Override
+    public void createToolWindowContent(@NotNull Project project, @NotNull ToolWindow toolWindow) {
+        jFXPanel = new JFXPanel();
+        ContentFactory contentFactory = ContentFactory.SERVICE.getInstance();
+        Content content = contentFactory.createContent(jFXPanel, "inspector", false);
+        toolWindow.getContentManager().addContent(content);
+
+        FXMLLoader fxmlLoader = new FXMLLoader();
+        fxmlLoader.setLocation(getClass().getResource("/layouts/AuthLayout.fxml"));
+
+        Platform.setImplicitExit(false);
+        PlatformImpl.runLater(() -> {
+            try {
+                Scene scene = new Scene(fxmlLoader.load());
+                jFXPanel.setScene(scene);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        });
+    }
+
+    // TODO: 10/23/2018 make gui great again
+
+}
