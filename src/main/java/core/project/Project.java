427f4e5f4ebc583b895bde17b1564cf0aae7be23
MikhailMe <neon-pf@yandex.ru>
2018-10-30 01:18:06 +0300

    i am debil, parsed submissions v0.1a

--- src/main/java/core/project/Project.java
+++ src/main/java/core/project/Project.java
@@ -1,4 +1,10 @@
 package core.project;
 
+import lombok.Data;
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+
+@Data
+@JsonDeserialize
 public class Project {
+
 }
--- src/main/java/core/sumbission/Denizen.java
+++ src/main/java/core/sumbission/Denizen.java
@@ -1,4 +1,25 @@
 package core.sumbission;
 
+import lombok.Data;
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+
+import java.util.List;
+
+@Data
+@JsonDeserialize
 public class Denizen {
+
+    private long id;
+    private String email;
+    private List<Profile> profiles;
+    private String denizenId;
+
+    @Override
+    public String toString() {
+        return "id = " + id + "\n" +
+                "email = " + email + "\n" +
+                "profiles = " + profiles.toString() + "\n" +
+                "denizenId = " + denizenId;
+    }
+
 }
